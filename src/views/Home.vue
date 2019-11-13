1292a150445ddc14d019918915d2e0090d55b111
Juliana Leme <julianaleme@gmail.com>
2019-11-13 08:13:08 -0300

    Commit com os logos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="logo" src="../assets/logo.png">
     <MyComp msg="Bem vindo! Este e o Vue.js App"/>
   </div>
 </template>
